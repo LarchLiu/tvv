f1b130ffcc76355666f93039c976101f657a3ad6
LarchLiu <LarchLiu@users.noreply.github.com>
2021-04-30 08:42:52 +0000

    deploy: 70c397c2afddad0198e558b2d489ad2bb21fb73f

---
+++ precache-manifest.78457e1d6812dd73a94c7579e490e5aa.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ba2ae710d927f13d483fd5d1e548c9b",
+    "url": "/gh-twitter/favicon.ico"
+  },
+  {
+    "revision": "f130a0b70e386170cf6f011c0ca8c4f4",
+    "url": "/gh-twitter/img/icons/android-chrome-192x192.png"
+  },
+  {
+    "revision": "0ff1bc4d14e5c9abcacba7c600d97814",
+    "url": "/gh-twitter/img/icons/android-chrome-512x512.png"
+  },
+  {
+    "revision": "845a39478d0e2d4d5d32a092de2de250",
+    "url": "/gh-twitter/img/icons/android-chrome-maskable-192x192.png"
+  },
+  {
+    "revision": "2695f5feb66cdb0c6f09d0e415824cf9",
+    "url": "/gh-twitter/img/icons/android-chrome-maskable-512x512.png"
+  },
+  {
+    "revision": "936d6e411cabd71f0e627011c3f18fe2",
+    "url": "/gh-twitter/img/icons/apple-touch-icon-120x120.png"
+  },
+  {
+    "revision": "1a034e64d80905128113e5272a5ab95e",
+    "url": "/gh-twitter/img/icons/apple-touch-icon-152x152.png"
+  },
+  {
+    "revision": "c43cd371a49ee4ca17ab3a60e72bdd51",
+    "url": "/gh-twitter/img/icons/apple-touch-icon-180x180.png"
+  },
+  {
+    "revision": "9a2b5c0f19de617685b7b5b42464e7db",
+    "url": "/gh-twitter/img/icons/apple-touch-icon-60x60.png"
+  },
+  {
+    "revision": "af28d69d59284dd202aa55e57227b11b",
+    "url": "/gh-twitter/img/icons/apple-touch-icon-76x76.png"
+  },
+  {
+    "revision": "66830ea6be8e7e94fb55df9f7b778f2e",
+    "url": "/gh-twitter/img/icons/apple-touch-icon.png"
+  },
+  {
+    "revision": "4bb1a55479d61843b89a2fdafa7849b3",
+    "url": "/gh-twitter/img/icons/favicon-16x16.png"
+  },
+  {
+    "revision": "98b614336d9a12cb3f7bedb001da6fca",
+    "url": "/gh-twitter/img/icons/favicon-32x32.png"
+  },
+  {
+    "revision": "b89032a4a5a1879f30ba05a13947f26f",
+    "url": "/gh-twitter/img/icons/msapplication-icon-144x144.png"
+  },
+  {
+    "revision": "058a3335d15a3eb84e7ae3707ba09620",
+    "url": "/gh-twitter/img/icons/mstile-150x150.png"
+  },
+  {
+    "revision": "4e857233cbd3bb2d2db4f78bed62a52f",
+    "url": "/gh-twitter/img/icons/safari-pinned-tab.svg"
+  },
+  {
+    "revision": "5ed23c203afe1f7de9ae7fc7ed168242",
+    "url": "/gh-twitter/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/gh-twitter/robots.txt"
+  },
+  {
+    "revision": "49c01f1c43702ee67df8",
+    "url": "/gh-twitter/static/css/app.cb8f91c0.css"
+  },
+  {
+    "revision": "69f81bb3a5bdf42300fa",
+    "url": "/gh-twitter/static/css/chunk-7be2e546.e3f562f7.css"
+  },
+  {
+    "revision": "eea7b2c3a48941616021",
+    "url": "/gh-twitter/static/css/chunk-a2fc6c3c.c066eb9e.css"
+  },
+  {
+    "revision": "0acb9a65b17ad573f319",
+    "url": "/gh-twitter/static/css/chunk-antdv.b50b2b7c.css"
+  },
+  {
+    "revision": "3fd90cf7cbc41cac9ced",
+    "url": "/gh-twitter/static/css/chunk-libs.5dd81f3c.css"
+  },
+  {
+    "revision": "49c01f1c43702ee67df8",
+    "url": "/gh-twitter/static/js/app.2bd5b16c.js"
+  },
+  {
+    "revision": "99e65bdf7502429097c4",
+    "url": "/gh-twitter/static/js/chunk-2d0a2d66.9635cc38.js"
+  },
+  {
+    "revision": "76e47527901f08260375",
+    "url": "/gh-twitter/static/js/chunk-55e11ae0.5e4a6eaa.js"
+  },
+  {
+    "revision": "69f81bb3a5bdf42300fa",
+    "url": "/gh-twitter/static/js/chunk-7be2e546.3e3d5682.js"
+  },
+  {
+    "revision": "eea7b2c3a48941616021",
+    "url": "/gh-twitter/static/js/chunk-a2fc6c3c.346e2b95.js"
+  },
+  {
+    "revision": "0acb9a65b17ad573f319",
+    "url": "/gh-twitter/static/js/chunk-antdv.b4094a15.js"
+  },
+  {
+    "revision": "3fd90cf7cbc41cac9ced",
+    "url": "/gh-twitter/static/js/chunk-libs.5304008b.js"
+  },
+  {
+    "revision": "c30dd15bb4d54a4df0b9",
+    "url": "/gh-twitter/static/js/runtime.d849d592.js"
+  }
+]);
